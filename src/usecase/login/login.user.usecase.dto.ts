d68f8caa9154558eaa989444ab42e911d1dd0fff
Ruan Ferraz <ruanlima155@gmail.com>
2024-10-15 22:07:50 -0300

    feat: add login use case

---
+++ src/usecase/login/login.user.usecase.dto.ts
@@ -0,0 +1,8 @@
+export interface InputLoginUserDto {
+  email: string;
+  password: string;
+}
+
+export interface OutputLoginUserDto {
+  token: string;
+}
---
+++ src/usecase/login/login.user.usecase.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import UserRepositoryInterface from "../../domain/repository/user.repository.interface";
+import {
+  InputLoginUserDto,
+  OutputLoginUserDto,
+} from "./login.user.usecase.dto";
+
+export default class LoginUseCase {
+  private userRepository: UserRepositoryInterface;
+
+  constructor(userRepository: UserRepositoryInterface) {
+    this.userRepository = userRepository;
+  }
+
+  async execute(input: InputLoginUserDto): Promise<OutputLoginUserDto> {
+    const user = await this.userRepository.findByEmail(input.email);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const isPasswordValid = await this.verifyPassword(
+      input.password,
+      user.password
+    );
+    if (!isPasswordValid) {
+      throw new Error("Invalid password");
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "8h",
+    });
+    return { token: token };
+  }
+
+  private async verifyPassword(
+    password: string,
+    userPassword: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, userPassword);
+  }
+}
