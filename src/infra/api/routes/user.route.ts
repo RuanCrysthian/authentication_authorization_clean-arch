7e62021f4da1bf1c6fe8a1c8c7a29f144400156e
Ruan Ferraz <ruanlima155@gmail.com>
2024-10-17 10:27:28 -0300

    feat: delete user endpoint

--- src/infra/api/routes/user.route.ts
+++ src/infra/api/routes/user.route.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import DeleteUserUseCase from "../../../usecase/delete/delete.user.usecase";
 import FindUserUseCase from "../../../usecase/find/find.user.usecase";
 import ListUserUseCase from "../../../usecase/list/list.user.usecase";
 import UserRepository from "../../repository/sequelize/user.repository.sequelize";
@@ -37,6 +38,26 @@ userRouter.get(
       const output = await useCase.execute({});
       res.status(200).send(output);
     } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
+
+userRouter.delete(
+  "/:id",
+  authMiddleware,
+  authorizationMiddleware(["admin"]),
+  async (req: Request, res: Response) => {
+    const useCase = new DeleteUserUseCase(new UserRepository());
+    try {
+      const input = { id: req.params.id };
+      await useCase.execute(input);
+      res.status(204).send();
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(404).send({ message: error.message });
+        return;
+      }
       res.status(500).send(error);
     }
   }
